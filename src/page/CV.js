a2f645dcc638eb8a83d4c0f694cd05f676da04f5
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-09-19 01:19:09 +0100

    first commit

---
+++ src/page/CV.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from "../component/Body";
+import Header from "../component/Header";
+
+const CV = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+export default CV;
